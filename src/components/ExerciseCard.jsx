206136de7f62209bb6121f6adaf4197cf89185e9
sujitmemane <officialsujitmemane@gmail.com>
2023-04-20 08:02:09 +0530

    change-2

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 const ExerciseCard = ({ gifUrl, name, exerciseId }) => {
-  useEffect(() => {
-    
-  }, [exerciseId]);
+  useEffect(() => {}, [exerciseId]);
   return (
     <Link to={`/exercises/${exerciseId}`}>
-      <div className="flex flex-col justify-between items-center cursor-pointer">
-        <img src={gifUrl} />
-        <h1 className="text-xl font-bold capitalize ">{name}</h1>
+      <div className="flex flex-col justify-between items-center cursor-pointer p-8 shadow-md">
+        <img src={gifUrl} className="h-44 w-44" />
+        <h1 className="text-sm font-bold capitalize ">{name}</h1>
       </div>
     </Link>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,70 @@
 import React, { useState } from "react";
 import Hero from "../components/Hero";
+const Dummy_Data = [
+  {
+    bodyPart: "biceps",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0436.gif",
+    id: "0436",
+    name: "dumbbell curl",
+    target: "biceps",
+  },
+  {
+    bodyPart: "back",
+    equipment: "barbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0658.gif",
+    id: "0658",
+    name: "bent-over barbell row",
+    target: "middle back",
+  },
+  {
+    bodyPart: "chest",
+    equipment: "body only",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0225.gif",
+    id: "0225",
+    name: "plyometric push-up",
+    target: "chest",
+  },
+  {
+    bodyPart: "triceps",
+    equipment: "cable",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0384.gif",
+    id: "0384",
+    name: "cable overhead triceps extension",
+    target: "triceps",
+  },
+  {
+    bodyPart: "shoulders",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0857.gif",
+    id: "0857",
+    name: "dumbbell lateral raise",
+    target: "shoulders",
+  },
 
+  {
+    bodyPart: "abs",
+    equipment: "body only",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0244.gif",
+    id: "0244",
+    name: "mountain climber",
+    target: "abs",
+  },
+];
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 const Home = () => {
-  const [exercises, setExercises] = useState([]);
- 
+  const [exercises, setExercises] = useState(Dummy_Data);
+
   return (
     <div>
       <Hero />
       <SearchExercises setExercises={setExercises} />
-      <Exercises exercises={exercises} />
+      {exercises.length === 0 ? (
+        "No Results Found"
+      ) : (
+        <Exercises exercises={exercises} />
+      )}
     </div>
   );
 };
