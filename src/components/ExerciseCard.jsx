1ed8b3e3f7209b319f46a25d9a08c508bf1ca8ee
sujitmemane <officialsujitmemane@gmail.com>
2023-04-20 09:58:01 +0530

    img-size-change

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -5,7 +5,7 @@ const ExerciseCard = ({ gifUrl, name, exerciseId }) => {
   return (
     <Link to={`/exercises/${exerciseId}`}>
       <div className="flex flex-col justify-between items-center cursor-pointer p-8 border-3 hover:shadow-md">
-        <img src={gifUrl} className="h-44 w-44" />
+        <img src={gifUrl} className="h-48 w-48" />
         <h1 className="text-sm font-bold capitalize ">{name}</h1>
       </div>
     </Link>
