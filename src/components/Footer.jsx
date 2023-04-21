197fa0474c6c11c33bb2635bf21adedf581db2b5
sujitmemane <officialsujitmemane@gmail.com>
2023-04-21 14:06:11 +0530

    minor-change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-black">
-      <div className="container mx-auto px-16 py-8">
+      <div className="container mx-auto md:px-16 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center text-white  ">
           <h1 className="text-white text-2xl cursor-pointer">GymAholic</h1>
-          <h2 className="text-white font-bold">
+          <h2 className="text-white  tracking-wide text-sm font-bold text-center">
             Developed By
             <a
               href="https://twitter.com/iAmSujitMemane"
               target="_blank"
-              className=" text-[#6557fd]"
+              className=" text-[#6557fd] mx-2"
             >
               The Sujit Memane
             </a>
